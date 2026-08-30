fd9eeb6bca5507e158fb834d0689afd606d63430
Maryamawit Samson <maryamawitsamson21@gmail.com>
2026-08-30 17:57:07 +0300

    Update weather.js

--- weather.js
+++ weather.js
@@ -67,12 +67,12 @@ else{lows.innerHTML=localStorage.getItem("min")+ " °C";}
 const weatherType = json.weather[0].main;
 
 if (weatherType === "Clear"){
-   type.src="sun.jpeg";}
+   type.src="./sun.jpeg";}
  else if (weatherType === "Clouds") {
-    type.src="cloud.png";
+    type.src="./cloud.png";
 }
 else if (weatherType === "Rain") {
-    type.src="rain.png";
+    type.src="./rain.png";
 }
 else{
     type.src= "img.png/search_9373594.png";
